70c18ef12e2ada8f33b314092b3ffe30488490ec
seongeun223 <jklun080921@gmail.com>
2024-06-11 16:02:23 +0900

    where

--- 02_MYSQL_practice1/03_WHERE_HW.sql
+++ 02_MYSQL_practice1/03_WHERE_HW.sql
@@ -63,4 +63,65 @@ select
     *
 from
     employee
+where phone not like '010%';
+
+select
+    *
+from
+    employee
+where DEPT_CODE in ('D1', 'D2', 'D3');
+
+select
+    *
+from
+    employee
+where JOB_CODE not in ('J1', 'J2', 'J3');
+
+select
+    *
+from
+    employee
+where SAL_LEVEL in ('S1', 'S2', 'S3');
+
+select
+    *
+from
+    employee
+where DEPT_CODE is null;
+
+select
+    *
+from
+    employee
+where email is not null;
+
+select
+    *
+from
+    employee
+where dept_code = 'D6' or 'D8' in (emp_name, salary);
+
+select
+    emp_name,
+    emp_id,
+    email
+from
+    employee
+where emp_name not like '이__';
+
+select
+    emp_name,
+    salary,
+    job_code
+from
+    employee
+where job_code = 'J2' and (salary >= 2000000) or job_code = 'J7';
+
+select
+    emp_name,
+    salary,
+    job_code
+from
+    employee
+where (job_code = 'J7' or job_code = 'J2') and salary >= 2000000;
 
